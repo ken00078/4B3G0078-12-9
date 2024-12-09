fe71570b10938f7d49caddec9ce4442821761d19
4b3g0078@office.stust.edu.tw <4b3g0078@office.stust.edu.tw>
2024-12-09 19:01:10 +0800

    新增衍生自Person的Student類別 新增列舉列別ClassName, Department 學會使用Visual Studio的除錯功能 學會使用git的分支(branch)功能 學會類別的繼承觀念

--- 4B3G0078-12-9/4B3G0078-12-9.cpp
+++ 4B3G0078-12-9/4B3G0078-12-9.cpp
@@ -1,6 +1,8 @@
 ﻿#include <string>
 #include <iostream>
 #include "Person.h"
+#include "Student.h"
+#include "Utility.h"
 using namespace std;
 
 int main()
@@ -17,4 +19,7 @@ int main()
 	person1.display();
 	cout << endl;
 	person2->display();
+
+	Student student1("A123456789", "Chen", "Jason", "M", "1999-01-01", "S001", Department::ComputerSciece, ClassName::_1A);
+	student1.display();
 }
